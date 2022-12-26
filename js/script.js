8a0569822952f0291207050dec783b15373688b4
Giox367 <111007060+Giox367@users.noreply.github.com>
2022-12-26 13:13:50 +0100

    .

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// NODEJS & TYPESCRIPT // L'oggetto Math // 2 Giorni
+// NPM, gestione pacchetti
+// Sintassi Typescript
+var nome = 'Giovanni'; // Come in Js inizializzo una variabile con la keyword let, e assegno un valore
+nome = 'Rosario'; // Essendo 'nome' una variabile posso riassegnarne il valore MA!... 
+nome = 15; // QUI RICEVO ERRORE! 
+var nome2 = 'Giovanni';
+var eta = 30;
+var coniugato = true;
+var variabile;
+variabile = 'Giovanni'; // !ATTENZIONE! Se dichiaro una variabile di tipo null sarà null per tutto il blocco di codice, uguale con undefinded
+var obj = { nome: 'giovanni' }; // ATTENZIONE! REFERENCIES!
---
+++ script.ts
@@ -0,0 +1,17 @@
+// NODEJS & TYPESCRIPT // L'oggetto Math // 2 Giorni
+// NPM, gestione pacchetti
+// Sintassi Typescript
+
+let nome = 'Giovanni'; // Come in Js inizializzo una variabile con la keyword let, e assegno un valore
+nome = 'Rosario'; // Essendo 'nome' una variabile posso riassegnarne il valore MA!... 
+nome = 15; // QUI RICEVO ERRORE! 
+
+let nome2:string = 'Giovanni';
+let eta:number = 30;
+let coniugato: boolean = true;
+let variabile: null;
+variabile = 'Giovanni' // !ATTENZIONE! Se dichiaro una variabile di tipo null sarà null per tutto il blocco di codice, uguale con undefinded
+
+let obj = {nome : 'giovanni'}; // ATTENZIONE! REFERENCIES!
+
+
